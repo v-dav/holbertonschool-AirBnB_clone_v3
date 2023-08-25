7960849eca341dfc2ce64358ecb720fd18292b18
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-08-25 03:41:17 -0700

    add post API action to the app url route

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -21,9 +21,8 @@ def get_amenities_place(place_id):
     return jsonify(json_format)
 
 
-
-@app_views.route("/places/<place_id>/amenities/<amenity_id>", methods=["DELETE"],
-                 strict_slashes=False)
+@app_views.route("/places/<place_id>/amenities/<amenity_id>",
+                 methods=["DELETE"], strict_slashes=False)
 def delete_amenityPlaceId(place_id, amenity_id):
     """Deletes amenity object in Place if found"""
     the_place = storage.get(Place, place_id)
@@ -34,40 +33,30 @@ def delete_amenityPlaceId(place_id, amenity_id):
     if the_amenity is None:
         abort(404)
 
-    if the_place not in the_amenity.place_amenities:
+    if the_amenity not in the_place.amenities:
         abort(404)
     storage.delete(the_amenity)
     storage.save()
     return {}, 200
 
 
-# @app_views.route("/places/<place_id>/reviews", methods=["POST"],
-#                  strict_slashes=False)
-# def create_review(place_id):
-#     """Creates a review"""
+@app_views.route("/places/<place_id>/amenities/<amenity_id>", methods=["POST"],
+                 strict_slashes=False)
+def create_amenityInPlace(place_id, amenity_id):
+    """Creates an amenity linked to a place"""
 
-#     """Check and get existing place"""
-#     the_place = storage.get(Place, place_id)
-#     if the_place is None:
-#         abort(404)
+    the_place = storage.get(Place, place_id)
+    if the_place is None:
+        abort(404)
 
-#     """Check HTTP body request"""
-#     http_body_request = request.get_json(silent=True)
-#     if http_body_request is None:
-#         return "Not a JSON\n", 400
-#     elif "user_id" not in http_body_request:
-#         return "Missing user_id\n", 400
-#     elif "text" not in http_body_request:
-#         return "Missing text\n", 400
-#     else:
-#         """Check user_id"""
-#         the_user = storage.get(User, http_body_request["user_id"])
-#         if the_user is None:
-#             abort(404)
-#         http_body_request["place_id"] = place_id
+    the_amenity = storage.get(Amenity, amenity_id)
+    if the_amenity is None:
+        abort(404)
 
-#         """Create new review"""
-#         new_review = Review(**http_body_request)
-#         storage.new(new_review)
-#         storage.save()
-#         return jsonify(new_review.to_dict()), 201
+    if the_amenity in the_place.amenities:
+        return jsonify(the_amenity.to_dict()), 200
+
+    new_amenity = Amenity(id=amenity_id)
+    storage.new(new_amenity)
+    storage.save()
+    return jsonify(new_amenity.to_dict()), 201
